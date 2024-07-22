a96d4bf3858cf67e24dce8ad506bc3e713649ebf
Adan Vela <adan.vela@ucf.edu>
2024-07-22 17:11:34 -0400

    Updated version with new replaceZeros using system epsilon Maybe try to save epsilon as global variable instead of in class instance

--- MyHMM_Setters.py
+++ MyHMM_Setters.py
@@ -1,11 +1,11 @@
-from cfnc import replaceZeros
+from cfcn import replaceZeros
 from numpy import log
 
 def setT(self,T):
     self.T=T
-    self.log_T=log(  replaceZeros(T)  )
+    self.log_T=log(  replaceZeros(T, self.epsilon)  )
             
 def setPi0(self, pi0):
     pi0=pi0/pi0.sum()
     self.pi0=pi0
-    self.log_pi0=log(  replaceZeros(pi0)  )
+    self.log_pi0=log(  replaceZeros(pi0, self.epsilon)  )
